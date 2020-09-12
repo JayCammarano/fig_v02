cbae60eff02858e47f4ef0d18293f86e5d2ebbce
JayCammarano <jay.cammarano@gmail.com>
2020-09-12 18:59:53 -0400

    got reviews pulled from p4k

--- app/javascript/react/components/releases/show/ReleaseDescription.js
+++ app/javascript/react/components/releases/show/ReleaseDescription.js
@@ -1,15 +1,30 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import ArtistTile from "../../artists/ArtistTile";
 import ArtistInfoModal from "../../artists/ArtistInfoModal";
 const ReleaseDescription = (props) => {
   const [showHide, setShowHide] = useState(null);
-  const [artistModalID, setartistModalID] = useState(null)
+  const [artistModalID, setartistModalID] = useState(null);
 
   const artistModalTrigger = (artistID) => {
-     setartistModalID(artistID)
+    setartistModalID(artistID);
     setShowHide("is-active");
   };
+  
 
+  useEffect(() => {
+    document.getElementById("review").innerHTML = props.description[0]
+  }, [props.description])
+
+  const reviewSwitcher = () => {
+    debugger
+    if(document.getElementById("review").innerHTML === props.description[1]){
+      debugger
+      document.getElementById("review").innerHTML = props.description[0]}
+      else{
+        document.getElementById("review").innerHTML = props.description[1]
+        debugger
+      }
+  }
   const artistListingArray = props.artists.map((artist) => {
     return (
       <div
@@ -34,9 +49,19 @@ const ReleaseDescription = (props) => {
         <div className="card has-background-light">
           <h4 className="card-header-title has-text-dark">{props.name}</h4>
           <p className="has-text-dark has-text-weight-bold is-size-4 p-l-lg p-b-md">
-            Description:
+            Review from Pitchfork:
           </p>
-          <p className="has-text-dark p-b-lg p-l-lg p-r-lg">{props.description}</p>
+          <p id="review" className="has-text-dark p-b-lg p-l-lg p-r-lg overflow">
+      
+          </p>
+          
+          <p className="card-footer p-l-lg p-b-md">
+            <a className="card-footer-item has-text-dark" onClick={reviewSwitcher}>Pitchfork</a>
+            <a className="card-footer-item has-text-dark" onClick={reviewSwitcher}>Metacritic</a>
+            <a className="card-footer-item has-text-dark" onClick={reviewSwitcher}>Discogs</a>
+
+          </p>
+
         </div>
       </div>
       <p className="has-text-light has-text-weight-bold is-size-4 p-l-lg">
--- app/serializers/release_artists_serializer.rb
+++ app/serializers/release_artists_serializer.rb
@@ -1,4 +1,5 @@
 class ReleaseArtistsSerializer < ActiveModel::Serializer
+
   attributes :id, :title, :release_type, :embed_url, :description, :original_release_year,:relatedArtists, :relatedLabels, :embed_url,  :search_for_review
   require "my-p4k-ruby"
   require "nokogiri"
@@ -10,10 +11,22 @@ class ReleaseArtistsSerializer < ActiveModel::Serializer
     require 'uri'
 
   def search_for_review
-    response = HTTParty.get("https://pitchfork.com/reviews/albums/18105-chance-the-rapper-acid-rap/")
+    artist = URI.encode(object.artists.first.name)
+    album = URI.encode(object.title)
+    pitchfork_object = []
+    search = "https://pitchfork.com/search/?query=#{artist}%20#{album}"
+    response = HTTParty.get(search)
     response = Nokogiri::HTML.parse(response)    
-    response_body = response.xpath("//*[@id='review-article-5929e666eb335119a49ef35f']/div[2]/div[2]/div/div[1]/div/p")
-    response_parsed = response_body.to_s    
+    search_response = response.xpath('//*[@id="result-albumreviews"]/ul/li/div/a').map { |link| link['href'] }
+    review_URL = "https://pitchfork.com#{search_response[0]}"
+    review_response = HTTParty.get(review_URL)    
+    parsed_review = Nokogiri::HTML.parse(review_response)    
+    review_digest = parsed_review.xpath("//*[@id='review-article-5929e666eb335119a49ef35f']/div[2]/div[2]/div/div[1]/div/p")
+    review_digest_parsed = ActionController::Base.helpers.strip_links(review_digest.to_s)
+    full_review = parsed_review.xpath('//*[@id="review-article-5929e666eb335119a49ef35f"]/div[2]/div[2]/div/div[2]/div[1]/div[2]')
+    full_review_parsed = ActionController::Base.helpers.strip_links(full_review.to_s)
+    pitchfork_object << review_digest_parsed
+    pitchfork_object << full_review_parsed
   end
   def relatedArtists
     object.artists.each do |artist|
