fc159974e6723c9984c6755cafa65092f446c40f
JayCammarano <jay.cammarano@gmail.com>
2020-09-19 19:42:22 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,5 +7,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "https://fig-music.herokuapp.com/"
     resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head], credentials: true
   end
+  allow do
+    origins "https://www.youtube.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head], credentials: true
+  end
 
 end
