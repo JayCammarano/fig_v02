8dbd291432f94a57e1e4de8362b52502e8dba94b
JayCammarano <jay.cammarano@gmail.com>
2020-09-19 19:52:03 -0400

    amazon carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,14 @@
 CarrierWave.configure do |config|
   if Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-      config.enable_processing = false
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    }
+    if Rails.env.production?
+      config.fog_directory  = ENV["S3_BUCKET"]
+    else
+      config.fog_directory  = ENV["S3_BUCKET"]
     end
    
     # make sure our uploader is auto-loaded
