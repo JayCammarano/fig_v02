27b28a42184ddd74c6d7e38a63d841008e15b067
JayCammarano <jay.cammarano@gmail.com>
2020-09-25 20:42:50 -0400

    fixed tests

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe RegistrationsController, type: :controller do
     context "when a post is made with correct params" do
       it "adds a new user to the database" do
         previous_count = User.count
-        post :create, params: {"user"=>{"email"=>"test@gmail.com", "password"=>"testtest", "password_confirmation"=>"testtest"}}
+        post :create, params: {"user"=>{"username"=> "test", "email"=>"test@gmail.com", "password"=>"testtest", "password_confirmation"=>"testtest"}}
         new_count = User.count
 
         expect(response.status).to eq 200
@@ -14,7 +14,7 @@ RSpec.describe RegistrationsController, type: :controller do
       end
 
       it "returns the user object" do
-        post :create, params: {"user"=>{"email"=>"test@gmail.com", "password"=>"testtest", "password_confirmation"=>"testtest"}}
+        post :create, params: {"user"=>{"username"=> "test","email"=>"test@gmail.com", "password"=>"testtest", "password_confirmation"=>"testtest"}}
         returned_json = JSON.parse(response.body)      
 
         expect(response.status).to eq 200
@@ -32,7 +32,7 @@ RSpec.describe RegistrationsController, type: :controller do
 
         expect(response.status).to eq 400
         expect(new_count).to eq(previous_count)
-        expect(returned_json["error"]).to eq(["Password can't be blank", "Email can't be blank"])
+        expect(returned_json["error"]).to eq(["Password can't be blank", "Email can't be blank", "Username can't be blank"])
       end
     end
   end
