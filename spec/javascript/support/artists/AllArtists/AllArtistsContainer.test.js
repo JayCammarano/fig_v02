ae5c4c55991e2c2e5e905664190c9cf5dd8f8aeb
JayCammarano <jay.cammarano@gmail.com>
2020-09-26 17:07:19 -0400

    tseting Artist New Modal

--- spec/javascript/support/artists/AllArtists/AllArtistsContainer.test.js
+++ spec/javascript/support/artists/AllArtists/AllArtistsContainer.test.js
@@ -2,17 +2,17 @@ import React from "react";
 import Enzyme, { mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import fetchMock from "fetch-mock";
-import { act } from "react-dom/test-utils"
+import { act } from "react-dom/test-utils";
 import { BrowserRouter } from "react-router-dom";
 
 import AllArtistsContainer from "../../../../../app/javascript/react/components/artists/AllArtists/AllArtistsContainer";
 import ArtistPlaceHolderTile from "../../../../../app/javascript/react/components/artists/AllArtists/ArtistPlaceholderTile";
 import ArtistNewModal from "../../../../../app/javascript/react/components/artists/NewArtist/ArtistNewModal";
-import PleaseLogin from "../../../../../app/javascript/react/components/Auth/PleaseLogin"
+import PleaseLogin from "../../../../../app/javascript/react/components/Auth/PleaseLogin";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("AllArtistsContainer", () => {
-  let wrapper, artists;
+  let wrapper, artists, loggedOn;
 
   beforeEach(() => {
     artists = [
@@ -40,6 +40,11 @@ describe("AllArtistsContainer", () => {
         <AllArtistsContainer />
       </BrowserRouter>
     );
+    loggedOn = mount(
+      <BrowserRouter>
+        <AllArtistsContainer loggedInStatus="LOGGED_IN" />
+      </BrowserRouter>
+    );
   });
   afterEach(fetchMock.restore);
 
@@ -48,52 +53,88 @@ describe("AllArtistsContainer", () => {
   });
 
   it("displays the artist tiles", () => {
-    expect(wrapper.find({children: artists[0].name})).toExist
-    expect(wrapper.find({children: artists[1].name})).toExist
+    expect(wrapper.find({ children: artists[0].name })).toExist;
+    expect(wrapper.find({ children: artists[1].name })).toExist;
   });
 
   it("displays pleaselogin modal", () => {
     act(() => {
-      wrapper
-        .find(ArtistPlaceHolderTile)
-        .simulate('click');
-    })
-    wrapper.update()
-    expect(wrapper.find(PleaseLogin).props().showStatus).toEqual("is-active")
-
+      wrapper.find(ArtistPlaceHolderTile).simulate("click");
+    });
+    wrapper.update();
+    expect(wrapper.find(PleaseLogin).props().showStatus).toEqual("is-active");
   });
   it("closes the pleaselogin onClick of cancel button", () => {
     act(() => {
-      wrapper
-        .find(ArtistPlaceHolderTile)
-        .simulate('click');
-    })
-    wrapper.update()
+      wrapper.find(ArtistPlaceHolderTile).simulate("click");
+    });
+    wrapper.update();
 
     act(() => {
       wrapper
-        .find(PleaseLogin).find({children: "Close"})
-        .simulate('click');
-    })
-    wrapper.update()
+        .find(PleaseLogin)
+        .find({ children: "Close" })
+        .simulate("click");
+    });
+    wrapper.update();
 
-    expect(wrapper.find(PleaseLogin).props().showStatus).toEqual("")
-
+    expect(wrapper.find(PleaseLogin).props().showStatus).toEqual("");
   });
   it("closes the pleaselogin modal onClick of X", () => {
     act(() => {
-      wrapper
-        .find(ArtistPlaceHolderTile)
-        .simulate('click');
-    })
-    wrapper.update()
+      wrapper.find(ArtistPlaceHolderTile).simulate("click");
+    });
+    wrapper.update();
     act(() => {
       wrapper
-        .find(ArtistNewModal).find(".delete")
-        .simulate('click');
-    })
-    wrapper.update()
+        .find(ArtistNewModal)
+        .find(".delete")
+        .simulate("click");
+    });
+    wrapper.update();
 
-    expect(wrapper.find(ArtistNewModal).props().showModal).toEqual("")
+    expect(wrapper.find(ArtistNewModal).props().showModal).toEqual("");
   });
+
+  it("displays ArtistNewModal modal when logged in", () => {
+    act(() => {
+      loggedOn.find(ArtistPlaceHolderTile).simulate("click");
+    });
+    loggedOn.update();
+
+    expect(loggedOn.find(AllArtistsContainer).find(ArtistNewModal).props().showModal).toEqual("is-active")
+  });
+  it("closes the ArtistNewModal modal onClick of X", () => {
+    act(() => {
+      loggedOn.find(ArtistPlaceHolderTile).simulate("click");
+    });
+    loggedOn.update();
+    act(() => {
+      loggedOn
+        .find(ArtistNewModal)
+        .find(".delete")
+        .simulate("click");
+    });
+    loggedOn.update();
+
+    expect(loggedOn.find(ArtistNewModal).props().showModal).toEqual("");
+  });
+
+  it("closes the ArtistNewModal modal onClick of Cancel button", () => {
+    act(() => {
+      loggedOn.find(ArtistPlaceHolderTile).simulate("click");
+    });
+    loggedOn.update();
+    act(() => {
+      loggedOn
+        .find(ArtistNewModal)
+        .find({children: "Cancel"})
+        .simulate("click");
+    });
+    loggedOn.update();
+
+    expect(loggedOn.find(ArtistNewModal).props().showModal).toEqual("");
+  });
+
+
 });
